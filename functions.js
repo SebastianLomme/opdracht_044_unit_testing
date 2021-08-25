f3221b10dcf0d81cef0808dc477d7c25002d9a99
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-08-25 14:18:17 +0200

    opdracht unit testen

---
+++ functions.js
@@ -0,0 +1,18 @@
+const functions = {
+    add: (num1, num2) => num1 + num2,
+    multiplay: (num1, num2) => num1 * num2,
+    subtract: (num1, num2) => num1 - num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = {
+            firstName: "Brad"
+        }
+        user['lastName'] = 'Traversy'
+        return user
+    }
+
+}
+
+module.exports = functions;
+
---
+++ functions.test.js
@@ -0,0 +1,45 @@
+const functions = require('./functions')
+
+test("adds 2 + 2 to equal 4", () => {
+    expect(functions.add(2, 2)).toBe(4)
+})
+
+test("adds 2 + 2 to equal 4", () => {
+    expect(functions.add(2, 2)).not.toBe(5)
+})
+
+test("multiplayed 2 * 10 to equal 20 ", () => {
+    expect(functions.multiplay(2, 10)).toBe(20)
+})
+
+test("subtract 10 - 5 equal 5", () => {
+    expect(functions.subtract(10, 5)).toBe(5)
+})
+
+test("zou null moeten zijn", () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+test("checkValue Should be falsy when argument is undefined", () => {
+    const x = 5
+    expect(functions.checkValue()).toBeUndefined()
+})
+
+test("User should be Brad Traversy object", () => {
+    expect(functions.createUser()).toEqual({ firstName: 'Brad', lastName: 'Traversy' })
+})
+
+test("zou onder of gelijk aan 1600 zijn", () => {
+    const load1 = 800
+    const load2 = 800
+    expect(load1 + load2).toBeLessThanOrEqual(1600)
+})
+
+test("There is no I in team", () => {
+    expect("team").not.toMatch("I")
+})
+
+test("Admin should be in usernames", () => {
+    usernames = ["john", "karen", "admin"];
+    expect(usernames).toEqual(expect.arrayContaining(["admin"]))
+})
